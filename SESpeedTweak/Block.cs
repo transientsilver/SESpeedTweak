f1aa6b6f0c7ecb1dd67f08624507da31533ac837
transientsilver <transientsilver@gmail.com>
2014-06-02 18:04:01 -0400

    First Commit

---
+++ SESpeedTweak/Block.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SESpeedTweak
+{
+    public class Block
+    {
+        private XElement xe;
+
+        public Block(XElement e)
+        {
+            xe = e;
+        }
+
+        public decimal BuildTime
+        {
+            get
+            {
+                return Math.Round(Convert.ToDecimal(xe.Element("BuildTimeSeconds").Value), 1);
+            }
+
+            set
+            {
+                xe.SetElementValue("BuildTimeSeconds", value);
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return xe.Element("BlockPairName").Value;
+            }
+
+            set
+            {
+                xe.SetElementValue("BlockPairName", value);
+            }
+        }
+
+        public string Size
+        {
+            get
+            {
+                return xe.Element("CubeSize").Value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return Name + " - " + Size;
+            }
+        }
+
+    }
+}
---
+++ SESpeedTweak/BlockManger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SESpeedTweak
+{
+    class BlockManger
+    {
+        private XDocument xdoc;
+        public List<Block> blocks { get; set; }        
+        private string filename;
+
+        public BlockManger(string filepath)
+        {
+            filename = filepath;
+            loadData();
+        }
+
+        private void loadData()
+        {
+            xdoc = XDocument.Load(filename);
+
+            var allblocks = from b in xdoc.Descendants("Definition")
+                            select new Block(b);
+
+            blocks = allblocks.OrderBy(b => b.Name).ToList();
+        }
+
+        public void increaseSpeedByMultiplier(decimal multiplier)
+        {
+            foreach(Block b in blocks)
+            {
+                b.BuildTime = b.BuildTime / Math.Abs(multiplier);
+            }
+        }
+
+        public void decreaseSpeedByMultiplier(decimal multiplier)
+        {
+            foreach (Block b in blocks)
+            {
+                b.BuildTime = b.BuildTime * Math.Abs(multiplier);
+            }
+        }
+
+        public void resetBlocks()
+        {
+            xdoc = null;
+            blocks = null;
+
+            loadData();            
+        }
+
+        public void Save()
+        {
+            xdoc.Save(filename);
+        }
+
+    }
+}
